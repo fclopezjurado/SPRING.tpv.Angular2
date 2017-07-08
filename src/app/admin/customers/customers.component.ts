d155104b2cedf1c017292077fb567215dc1ea0f3
Francisco López <fclopezjurado@gmail.com>
2017-07-08 11:49:30 +0200

    #deleteMockups Fixes in definition of "CustomersComponent" component for "Customers" sub-module to delete "endpoint" attribute, because now it's not necessary.

--- src/app/admin/customers/customers.component.ts
+++ src/app/admin/customers/customers.component.ts
@@ -3,19 +3,17 @@
  */
 
 import {Component} from '@angular/core';
-import {USERS_URI, CUSTOMERS} from '../admin.config'
+import {CUSTOMERS} from '../admin.config'
 import {ROLE_CUSTOMER} from '../../app.config'
 
 @Component({
     templateUrl: './customers.component.html'
 })
 export class CustomersComponent {
-    endpoint: string;
     usersType: string;
     role: string;
 
     constructor() {
-        this.endpoint = USERS_URI;
         this.usersType = CUSTOMERS;
         this.role = ROLE_CUSTOMER;
     }
