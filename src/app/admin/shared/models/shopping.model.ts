24748ac0ac87346d9909db235f0543d19147d35f
Francisco López <fclopezjurado@gmail.com>
2017-07-08 12:12:02 +0200

    #deleteMockups Fixes in definition of "Shopping" model to delete "ticket" attribute and add "product", "productCode" and "retailPrice" attribute. These changes are caused by problems with data structure in response of server-side endpoints, because each endpoint has a different data structure.

--- src/app/admin/shared/models/shopping.model.ts
+++ src/app/admin/shared/models/shopping.model.ts
@@ -2,22 +2,25 @@
  * Created by fran lopez on 09/06/2017.
  */
 
-export const AMOUNT_ATTRIBUTE_NAME: string = 'amount';
-export const DISCOUNT_ATTRIBUTE_NAME: string = 'discount';
-export const DESCRIPTION_ATTRIBUTE_NAME: string = 'description';
-export const PRICE_ATTRIBUTE_NAME: string = 'price';
-export const STATE_ATTRIBUTE_NAME: string = 'state';
-export const CODE_ATTRIBUTE_NAME: string = 'code';
-export const TICKET_ATTRIBUTE_NAME: string = 'ticket';
+import {Product} from "../../../shared/models/product.model";
+
+export const AMOUNT_ATTRIBUTE_NAME = 'amount';
+export const DISCOUNT_ATTRIBUTE_NAME = 'discount';
+export const DESCRIPTION_ATTRIBUTE_NAME = 'description';
+export const PRICE_ATTRIBUTE_NAME = 'price';
+export const STATE_ATTRIBUTE_NAME = 'shoppingState';
+export const CODE_ATTRIBUTE_NAME = 'code';
 
 export class Shopping {
     constructor(public id: number, public amount: number, public discount: number, public description: string,
-                public price: number, public state: string, public code: string, public ticket: number) {
+                public price: number, public shoppingState: string, public code: string, public product: Product,
+                public productCode?: string, public retailPrice?: number) {
     }
 
     equals(shopping: Shopping): boolean {
         return ((shopping.id === this.id) && (shopping.amount === this.amount) && (shopping.discount === this.discount)
         && (shopping.description === this.description) && (shopping.price === this.price)
-        && (shopping.state === this.state) && (shopping.code === this.code) && (shopping.ticket === this.ticket))
+        && (shopping.shoppingState === this.shoppingState) && (shopping.code === this.code)
+        && this.product.equals(shopping.product))
     }
 }
