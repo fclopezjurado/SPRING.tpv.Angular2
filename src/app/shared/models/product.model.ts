b0670b72468ea4e1c29e3cef1ee010117e72ed5b
Francisco López <fclopezjurado@gmail.com>
2017-07-08 12:01:50 +0200

    #deleteMockups Fixes in definition of "Product" shared model to add a new function called "equals". This function is used to compare instances of "Product" model.

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -1,9 +1,20 @@
 /**
-  * @author Sergio Banegas Cortijo
-  * Github: https://github.com/sergiobanegas 
-*/
+ * @author Sergio Banegas Cortijo
+ * Github: https://github.com/sergiobanegas
+ * @author Fran López
+ * Github: https://github.com/fclopezjurado
+ */
+
 export class Product {
     constructor(public code: string, public reference: string, public description: string,
-        public retailPrice: number, public discontinued: boolean, public image: string,
-        public longDescription: string) { }
+                public retailPrice: number, public discontinued: boolean, public image: string,
+                public longDescription: string) {
+    }
+
+    equals(product: Product): boolean {
+        return ((product.code === this.code) && (product.reference === this.reference)
+        && (product.description === this.description) && (product.retailPrice === this.retailPrice)
+        && (product.discontinued === this.discontinued) && (product.image === this.image)
+        && (product.longDescription === this.longDescription))
+    }
 }
